6b7104aec788e3e169b15ba1e771d229124e95e7
shalk <xshalk@163.com>
2023-12-04 15:37:35 +0800

    add workflow

---
+++ .github/workflow/download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e -x
+
+PWD=$(pwd)
+cd "$(dirname "$0")" && cd ../../
+
+date_format=$(date +%Y-%m-%d)
+dir="history/$date_format"
+url="https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml"
+newurl="https://shalk.github.io/openai-swagger-ui/$dir/openapi.yaml"
+
+mkdir -p "$dir"
+rm -f "$dir/openapi.yaml" "$dir/index.html"
+
+if ! curl "$url" -o "$dir/openapi.yaml"; then
+    echo "Error downloading openapi.yaml from $url"
+    exit 1
+fi
+
+cp -f index.html "$dir/index.html"
+
+sed -i "s#$url#$newurl#" "$dir/index.html"
+
+cd "$PWD"
---
+++ README.md
@@ -0,0 +1,17 @@
+openai-swagger-ui
+====
+
+this is Web UI interface to see OpenAI' latest REST API.
+
+
+open https://shalk.github.io/openai-swagger-ui/ 
+
+## history
+
+The OpenAI REST API maybe changed, just keep the snapshot of API every day.
+
+example: https://shalk.github.io/openai-swagger-ui/history/2023-12-04/
+
+
+## LICENCE 
+apache license 2.0
